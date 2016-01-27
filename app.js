b942fc52cf6f9de9e3b4b3569b4ac777342d67e2
Benjamin Baka <baka@mpedigree.net>
2016-01-27 22:37:55 +0000

    Ben: call middleware

--- app.js
+++ app.js
@@ -1,6 +1,10 @@
 var http = require('http');
 var express = require('express');
+var forbidder = require('./forbidder.js');
 var app=express();
+
+app.use(forbidder("Wednesday"));
+
 
 http.createServer(app).listen(3000, function(){
     console.log("Express server listening on port "+ 3000);
