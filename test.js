510196555b471a4d9c8f078560180048bea1e595
Benjamin Baka <baka@mpedigree.net>
2016-01-27 22:38:31 +0000

    Ben: Playing with all manner of node modules

--- test.js
+++ test.js
@@ -1,7 +1,11 @@
 var mod = require("./mod.js");
+var reverse = require("./reverse.js");
 
 console.log(mod.name);
 console.log(mod.lower("APPLE"));
 console.log(mod.upper("mango"));
 console.log(mod.get_name());
 console.log(mod.get_secret());
+
+
+console.log(reverse('hippopotamus'));
