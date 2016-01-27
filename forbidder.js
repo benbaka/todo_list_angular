318c4b923bc3140cbe202226bc75244a3ac071b6
Benjamin Baka <baka@mpedigree.net>
2016-01-27 22:36:56 +0000

    Ben: simple middle ware

---
+++ forbidder.js
@@ -0,0 +1,16 @@
+module.exports = function(forbidden_day){
+    var days=['Sunday', 'Monday', 'Tuesday', 'Wednesday',
+        'Thursday', 'Friday', 'Saturday'
+    ];
+
+    return function(req, res, next){
+        var da = new Date().getDay();
+
+        if (days[day] === forbidden_day){
+            res.send("No visitors allowed on " + forbidden_day + "s!");
+        }
+        else{
+            next();
+        }
+    }
+};
