0fbd2204a203122c297c2a5b226ab2f9c0254529
Benjamin Baka <baka@mpedigree.net>
2016-01-29 21:44:48 +0000

    Ben: added view engine, jade and static folders

--- app.js
+++ app.js
@@ -3,6 +3,17 @@ var express = require('express');
 var forbidder = require('./forbidder.js');
 var app=express();
 
+// Set view engine
+app.set('view engine', 'jade');
+
+app.set('views', './views')
+
+// call the static views
+app.use(express.static("./public"));
+app.use(express.static("./files"));
+app.use(express.static("./downloads"));
+
+
 // Load the routes
 var routes = require("./routes")(app);
 
